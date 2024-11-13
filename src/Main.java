416683b99b0d49c8677da71829ed0308274d0c2d
BengRhui <limbengrhui3@gmail.com>
2024-11-13 15:25:58 +0800

    Update gitignore (not again)
    Hopefully this time gitignore works

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         // This code is used to see if the frontend backend package can run
-        System.out.println("Testing here:");
+        System.out.println("Testing:");
         new FrontendTest();
         new BackendTest();
 
