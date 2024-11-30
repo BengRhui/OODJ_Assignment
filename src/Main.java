dc59ca2c44630135320c65962c8d1031f983852d
BengRhui <limbengrhui3@gmail.com>
2024-11-30 22:24:46 +0800

    Test for Changes in Netbeans 2
    Seems like it is working, nice

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         // This code is used to see if the frontend backend package can run
-        System.out.println("Testing");
+        System.out.println("Testing:");
         new FrontendTest();
         new BackendTest();
 
