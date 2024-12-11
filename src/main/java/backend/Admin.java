5023f229be7c6ae6b600b5a5c79ccab3a3185999
BengRhui <limbengrhui3@gmail.com>
2024-12-11 15:39:01 +0800

    Create Admin class
    The admin class is created to represent admins.

---
+++ src/main/java/backend/Admin.java
@@ -0,0 +1,52 @@
+package backend;
+
+import java.util.ArrayList;
+
+/**
+ * Class {@code Admin} represents users with the Admin role in the system.<br>
+ * This class is inherited from the {@code User} class with the following attributes from the class:
+ * @author Beng Rhui (TP068495)
+ */
+public class Admin extends User {
+
+    /**
+     * A list that contains all instances of the {@code Admin} class.
+     */
+    private final static ArrayList<Admin> adminList = new ArrayList<>();
+
+    /**
+     * Constructor for the {@code Admin} class.
+     * @param user An instance of the {@code User} class whose user type is Admin
+     */
+    public Admin(String userID, String email, String password, String name) {
+        super(userID, email, password, name);
+        addToAdminList(this);
+    }
+
+    /**
+     * A method to return a list containing all objects from the {@code Admin} class.
+     * @return ArrayList with all {@code Admin} instances
+     */
+    public static ArrayList<Admin> getAdminList() {
+        return adminList;
+    }
+
+    /**
+     * A method to add an {@code Admin} object into an overall list.
+     */
+    public static void addToAdminList(Admin admin) {
+        adminList.add(admin);
+    }
+
+    /**
+     * A method to print out information about the {@code Admin} object.
+     * @return A string representation of the {@code Admin} object
+     */
+    @Override
+    public String toString() {
+        return "Admin ID: " + super.userID + "\n" +
+                "Admin Email: " + super.email + "\n" +
+                "Admin Password: " + super.password + "\n" +
+                "Admin Name: " + super.name;
+    }
+}
