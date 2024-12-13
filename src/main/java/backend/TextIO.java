9d62d418b14a464ef52abad2c1f3ffddd6bfec1c
BengRhui <limbengrhui3@gmail.com>
2024-12-13 19:31:57 +0800

    Create method to read file in general
    The method readFile() aims to read any text files in general. It returns a list containing the contents of the file.

--- src/main/java/backend/TextIO.java
+++ src/main/java/backend/TextIO.java
@@ -15,4 +15,44 @@ public class TextIO {
      */
     private final static String PARENT_PATH_TO_FILE = "src/main/resources/text_file/";
 
+    /**
+     * A method to read text files from directory
+     *
+     * @param fileName The name of the text file to be read
+     * @return An ArrayList containing the contents of the text file.<br>
+     * System exits automatically if the text file is not found.
+     */
+    public static ArrayList<String> readFile(String fileName) {
+
+        // Obtain the path to the file
+        String filePath = PARENT_PATH_TO_FILE + fileName.strip().toLowerCase();
+
+        try {
+
+            // Attempt to read file using FileReader and BufferedReader
+            FileReader file = new FileReader(filePath);
+            BufferedReader reader = new BufferedReader(file);
+
+            // Create local variables: array (store all lines) and string (store individual line during loop)
+            ArrayList<String> fileContents = new ArrayList<>();
+            String line;
+
+            // Loop through each line in the text file and add them to the array
+            while ((line = reader.readLine()) != null) {
+                fileContents.add(line);
+            }
+
+            // Close the file and return the array
+            file.close();
+            return fileContents;
+
+        } catch (IOException e) {
+
+            // Print out the file name that is not available and exit the system immediately
+            System.out.println(fileName + " file is not found.");
+            System.exit(0);
+        }
+
+        return null;
+    }
 }
