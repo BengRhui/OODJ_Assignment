4513e35cc840cbb300db9e2c47040f11861f56d2
BengRhui <limbengrhui3@gmail.com>
2024-12-13 17:58:38 +0800

    Create Customer Notification Class
    The customer notification class is created to model notifications from customer.

---
+++ src/main/java/backend/CustomerNotification.java
@@ -0,0 +1,140 @@
+package backend;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class CustomerNotification implements Notification{
+
+    /**
+     * Attributes for {@code CustomerNotification} class.<br>
+     * An overall list containing all {@code CustomerNotification} objects is included.<br>
+     * A public list which contains the read status is also included.
+     */
+    private String notificationID;
+    private Customer customer;
+    private LocalDateTime notificationTime;
+    private NotificationStatus readStatus;
+    private String notificationTitle;
+    private String notificationDetails;
+
+    private final static ArrayList<Notification> customerNotificationList = new ArrayList<>();
+
+    /**
+     * Constructor to instantiate {@code CustomerNotification} objects.
+     *
+     * @param notificationID      The ID of the notification
+     * @param customer              The customer associated with the notification
+     * @param notificationTime The time when the notification is created
+     * @param readStatus Status to record if customer has read the notification
+     * @param notificationTitle The title of the notification
+     * @param notificationDetails The description of the notification
+     */
+    public CustomerNotification(String notificationID, Customer customer, LocalDateTime notificationTime,
+                                NotificationStatus readStatus, String notificationTitle, String notificationDetails) {
+        this.notificationID = notificationID;
+        this.customer = customer;
+        this.notificationTime = notificationTime;
+        this.readStatus = readStatus;
+        this.notificationTitle = notificationTitle;
+        this.notificationDetails = notificationDetails;
+    }
+
+    /**
+     * Getters and setters for {@code CustomerNotification} class.
+     */
+    public String getNotificationID() {
+        return notificationID;
+    }
+
+    public void setNotificationID(String notificationID) {
+        this.notificationID = notificationID;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public LocalDateTime getNotificationTime() {
+        return notificationTime;
+    }
+
+    public void setNotificationTime(LocalDateTime notificationTime) {
+        this.notificationTime = notificationTime;
+    }
+
+    public NotificationStatus getReadStatus() {
+        return readStatus;
+    }
+
+    public void setReadStatus(NotificationStatus readStatus) {
+        this.readStatus = readStatus;
+    }
+
+    public String getNotificationTitle() {
+        return notificationTitle;
+    }
+
+    public void setNotificationTitle(String notificationTitle) {
+        this.notificationTitle = notificationTitle;
+    }
+
+    public String getNotificationDetails() {
+        return notificationDetails;
+    }
+
+    public void setNotificationDetails(String notificationDetails) {
+        this.notificationDetails = notificationDetails;
+    }
+
+    /**
+     * A method to retrieve the list containing all customer notifications.
+     * @return An ArrayList consisting of all {@code CustomerNotification} instances
+     */
+    public static ArrayList<Notification> getCustomerNotificationList() {
+        return customerNotificationList;
+    }
+
+    /**
+     * A method to add customer notifications into an overall list.
+     * @param notification The {@code CustomerNotification} object to be added to list
+     */
+    public static void addToList(CustomerNotification notification) {
+        customerNotificationList.add(notification);
+    }
+
+    /**
+     * A method to print out the full information for customer notifications.
+     * @return String representation of the {@code CustomerNotification} object
+     */
+    @Override
+    public String toString() {
+        return "Notification ID: " + notificationID + "\n" +
+                "Customer Involved: " + "\n" +
+                customer.toString() + "\n" +
+                "Notification Time: " + Utility.printTime(notificationTime) + "\n" +
+                "Notification Status: " + readStatus.toString() + "\n" +
+                "Notification Title: " + notificationTitle + "\n" +
+                "Notification Details: " + notificationDetails;
+    }
+    /**
+     * A method to retrieve the message portion of customer notifications.
+     * @return The message details of customer notification
+     */
+    @Override
+    public String getMessage() {
+        return "Notification Title: " + notificationTitle + "\n" +
+                "Notification Details: " + notificationDetails;
+    }
+
+    /**
+     * A method to switch the status of notifications from "unread" to "read".
+     */
+    @Override
+    public void markAsRead() {
+        this.readStatus = NotificationStatus.READ;
+    }
+}
