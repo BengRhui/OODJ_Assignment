53e73ecf916c071073838650e4a3e33a8b9612c5
BengRhui <limbengrhui3@gmail.com>
2024-12-13 19:28:48 +0800

    Created TextIO class
    The TextIO class is created to read and write text files from and to the system.

---
+++ src/main/java/backend/TextIO.java
@@ -0,0 +1,13 @@
+package backend;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Class {@code TextIO} contains all the methods to retrieve and write the contents of text files.
+ */
+public class TextIO {
+
+}
