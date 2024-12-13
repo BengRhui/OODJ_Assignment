e344853a8b86174ff59588fd1600a953f8bd5c76
BengRhui <limbengrhui3@gmail.com>
2024-12-13 21:10:31 +0800

    Create method to print array in array
    Another method is included to print string arrays in array

--- src/main/java/backend/Utility.java
+++ src/main/java/backend/Utility.java
@@ -2,6 +2,8 @@ package backend;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Class {@code Utility} includes a couple of methods that brings convenient to coding.
@@ -16,7 +18,6 @@ public class Utility {
      * @param time Time in {@code LocalDateTime} format
      * @return Time in String format with style of "dd/MM/yyyy HH:mm:ss"
      */
-    public static String printTime(LocalDateTime time) {
     public static String generateString(LocalDateTime time) {
 
         // Indicates the format of the time
@@ -24,5 +25,20 @@ public class Utility {
 
         // Returns the time in String format
         return time.format(timeFormat);
+    }
+
+    /**
+     * A method to print array-in-array in a readable format.
+     *
+     * @param array Array-in-array
+     * @return Contents of the array-in-array, with each array separated by lines
+     */
+    public static String generateString(ArrayList<String[]> array) {
+        StringBuilder string = new StringBuilder();
+        for (String[] stringList : array) {
+            string.append(Arrays.toString(stringList));
+            string.append("\n");
+        }
+        return string.toString();
     }
 }
