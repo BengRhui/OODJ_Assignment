54c51ecf37871f3d16cb15adecefac4187bb186c
BengRhui <limbengrhui3@gmail.com>
2024-12-14 20:27:23 +0800

    Create delivery runner file IO
    This class is to allow users to read from and write to delivery runner information file.

---
+++ src/main/java/backend/DeliveryRunnerFileIO.java
@@ -0,0 +1,83 @@
+package backend;
+
+import java.util.ArrayList;
+
+/**
+ * Class {@code DeliveryRunnerFileIO} contains the methods to read and write delivery runner files
+ * @author Beng Rhui (TP068495)
+ */
+public class DeliveryRunnerFileIO extends UserFileIO {
+
+    /**
+     * Fixed variables to help in coding
+     */
+    private final static String RUNNER_FILE_NAME = "delivery_runner_information.txt";
+    private final static int NUMBER_OF_INFO_IN_FILE = 3;
+    private final static int[] SPACING_SIZE = {5, 50, 15};
+
+    /**
+     * A method to read delivery runner file and initialize the delivery runner objects.
+     */
+    @Override
+    public void readFile() {
+
+        // Get list of runners from text file
+        ArrayList<String[]> runnerList = getListFromFile(RUNNER_FILE_NAME);
+
+        // Loop through the runners
+        for (String[] runnerRecord : runnerList) {
+
+            // Create the runners and add them to list
+            DeliveryRunner newRunner = createDeliveryRunnerObject(runnerRecord);
+            DeliveryRunner.addToRunnerList(newRunner);
+        }
+    }
+
+    /**
+     * A method to create delivery runner objects based on records stored as string array.
+     * @param individualRecord The string array containing the delivery runner's information
+     * @return The {@code DeliveryRunner} object being created
+     */
+    public static DeliveryRunner createDeliveryRunnerObject(String[] individualRecord) {
+
+        // Retrieve information from the string array
+        String userID = individualRecord[0];
+        String name = individualRecord[1];
+        String contactNumber = individualRecord[2];
+
+        // Retrieve email and password
+        String[] credentials = getUsernameAndPassword(userID);
+        String email = credentials[0];
+        String password = credentials[1];
+
+        // Create delivery runner object
+        return new DeliveryRunner(userID, email, password, name, contactNumber);
+    }
+
+    /**
+     * A method to write information into delivery runner file.
+     */
+    @Override
+    public void writeFile() {
+
+        // Get the list of delivery runners
+        ArrayList<DeliveryRunner> runnerList = DeliveryRunner.getDeliveryRunnerList();
+
+        // Instantiate a local variable to store the data to be written to file
+        ArrayList<String[]> information = new ArrayList<>();
+
+        // Loop through each delivery runner
+        for (DeliveryRunner runner : runnerList) {
+
+            // Retrieve information to be stored and add to array
+            String[] runnerInformation = new String[NUMBER_OF_INFO_IN_FILE];
+            runnerInformation[0] = runner.getUserID();
+            runnerInformation[1] = runner.getName();
+            runnerInformation[2] = runner.getContactNumber();
+            information.add(runnerInformation);
+        }
+
+        // Write the information into the file
+        writeListToFile(RUNNER_FILE_NAME, information, SPACING_SIZE);
+    }
+}
