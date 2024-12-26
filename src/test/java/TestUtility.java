4a601b32797c2c9b61e583a10709db80cf188b6a
BengRhui <limbengrhui3@gmail.com>
2024-12-26 22:58:40 +0800

    Add method to retrieve different items

--- src/test/java/TestUtility.java
+++ src/test/java/TestUtility.java
@@ -1,3 +1,4 @@
+import backend.entity.Item;
 import backend.notification.Notification;
 
 import java.util.ArrayList;
@@ -16,7 +17,7 @@ public class TestUtility {
      * @param newList     The list after modification takes place
      * @return An array consisting of the notification not in the initial list
      */
-    public static ArrayList<Notification> getDifferent(ArrayList<Notification> initialList, ArrayList<Notification> newList) {
+    public static ArrayList<Notification> getDifferentNotification(ArrayList<Notification> initialList, ArrayList<Notification> newList) {
 
         // Declare empty list to accommodate the different notifications
         ArrayList<Notification> differentList = new ArrayList<>();
@@ -57,5 +58,40 @@ public class TestUtility {
                         .map(notification -> (Notification) notification)     // Convert each element to a notification item
                         .toList()                                             // Return back to list
         );
+    }
+
+    /**
+     * A method to obtain the item objects that does not exist in the initial list.
+     * @param initialList The initial list containing item objects before modification
+     * @param newList The new list containing item objects after modification
+     * @return An array consisting of item objects not in initial list
+     */
+    public static ArrayList<Item> getDifferentItem(ArrayList<Item> initialList, ArrayList<Item> newList) {
+
+        // Declare an empty list to store different item objects
+        ArrayList<Item> differentList = new ArrayList<>();
+
+        // Loop through the items in the new list
+        for (Item newItem : newList) {
+
+            // Declare a variable to record whether item is in the list
+            boolean isInList = false;
+
+            // Loop through the initial list
+            for (Item initialItem : initialList) {
+
+                // Change boolean to true and end list if item ID matches
+                if (initialItem.getItemID().equals(newItem.getItemID())) {
+                    isInList = true;
+                    break;
+                }
+            }
+
+            // Add item to list if item is not in the list
+            if (!isInList) differentList.add(newItem);
+        }
+
+        // Return the list
+        return differentList;
     }
 }
