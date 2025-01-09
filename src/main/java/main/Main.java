1c2c24e42220eac1a96c5d9b820449e681024c35
BengRhui <limbengrhui3@gmail.com>
2025-01-10 01:11:40 +0800

    Modified main class since a GUI is created

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,15 +1,12 @@
 package main;
 
-import backend.Backend;
-import frontend.Frontend;
+import frontend.home_page.HomePage;
 
 public class Main {
 
     public static void main(String[] args) {
 
         // This code is used to see if the frontend backend package can run
-        System.out.println("Testing:");
-        new Frontend();
-        new Backend();
+        new HomePage().setVisible(true);
     }
 }
