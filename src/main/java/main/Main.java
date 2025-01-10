81c50c9216e6a8a0067aed397a81b56e40b77704
BengRhui <limbengrhui3@gmail.com>
2025-01-10 15:14:26 +0800

    Add method to read all files

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,14 @@
 package main;
 
+import backend.file_io.FileIO;
 import frontend.home_page.LoginPage;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        // Read all the files before running the system
+        FileIO.readAllFIle();
 
         // This code is used to see if the frontend backend package can run
         new LoginPage().setVisible(true);
