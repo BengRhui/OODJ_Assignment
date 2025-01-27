cb3e3c776507033cf128fa0a15a41290662e3cdb
BengRhui <limbengrhui3@gmail.com>
2025-01-28 02:21:26 +0800

    Remove unused code 2

--- src/main/java/backend/entity/Vendor.java
+++ src/main/java/backend/entity/Vendor.java
@@ -68,34 +68,6 @@ public class Vendor extends User {
     }
 
     /**
-     * A method to search for vendor using their names.
-     *
-     * @param name The name of the vendor
-     * @return An array list consisting of the vendors fulfilling the condition
-     */
-    public static ArrayList<Vendor> findVendorByName(String name) {
-
-        // Return the list (for empty strings, return back all values)
-        return vendorList.stream()
-                .filter(vendor -> vendor.getName().toLowerCase().contains(name.toLowerCase()))
-                .collect(Collectors.toCollection(ArrayList::new));
-    }
-
-    /**
-     * A method to get the relevant vendor list based on stall name.
-     *
-     * @param stallName The name of the stall
-     * @return The list of filtered vendors
-     */
-    public static ArrayList<Vendor> findVendorByStallName(String stallName) {
-
-        // Return the list (empty strings - return all values)
-        return vendorList.stream()
-                .filter(vendor -> vendor.getStall().getStallName().toLowerCase().contains(stallName.toLowerCase()))
-                .collect(Collectors.toCollection(ArrayList::new));
-    }
-
-    /**
      * A method to retrieve {@code Vendor} object using vendor ID
      *
      * @param vendorID The ID of the vendor
